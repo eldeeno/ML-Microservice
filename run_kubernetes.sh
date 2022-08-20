cca6d251a1e2cde190c5b382fc27413aef70c4f6
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2022-08-20 09:33:08 +0100

    Complete Docker file, Run a Container & Make a Prediction, Logging, Upload Docker image, Configure Kuber, Deploy Kuber

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=eldeeno/microservice-model:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy microservice-model123 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/microservice-model123-68d5879b6c-kzp5s --address 0.0.0.0 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=eldeeno/microservice-model:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag microservice-model $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+sudo docker login
+docker push $dockerpath
